bcc565a055f090a1f844895aff85fc002b6a431d
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2026-01-31 15:47:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,18 @@ function showData(data) {
   }
 }
 
+// Get prev and next songs
+async function getMoreSongs(url) {
+    const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+    const data = await res.json();
+
+    showData(data);
+}
+
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiURL}/vs/${artist}/${songTitle}`);
+    const data = await res.json();
+
+    
+}
